1391e90f2adff02e5d75f7cd35d7ebcd0f5ead36
Karl Stolley <karl.stolley@gmail.com>
2017-11-18 13:24:26 -0600

    Remove existing HTML and insert a `<button>`

--- pp-05/site.js
+++ pp-05/site.js
@@ -2,3 +2,8 @@ function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+
+(function($) {
+  $('figure').remove(); // remove the HTML
+  $('body').append('<button id="doorbell">Ring the Doorbell</button>'); // cheat a bit and use button
+})(jQuery);
