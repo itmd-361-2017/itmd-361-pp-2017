f3139e46b18a8224394b37f35e8cd0b52c18e799
Karl Stolley <karl.stolley@gmail.com>
2017-11-18 14:20:09 -0600

    Bonus task four: visually indicate doorbell ring
    It's events all the way down. Here I'm just registering listeners on the
    `doorbell` object for when it begins to play, and when its playback has
    ended. On those events, I just add a message to the DOM that the
    doorbell rings, and remove it as soon as playback is done.
    
    There are other approaches here, mostly with Promise objects that Audio
    is supposed to return -- but those are not as widely supported as good
    old media events. See the Mozilla docs on HTMLMediaElement and
    especially the Events section:
    
    https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,6 +1,16 @@
 (function($) {
   var doorbell = new Audio('media/doorbell.mp3');
 
+  // Register some event listeners on the doorbell
+  // Add a status to the DOM when playback begins:
+  doorbell.addEventListener('play', function() {
+    $('#doorbell').after('<p id="status">((( Doorbell Rings )))</p>');
+  });
+  // Remove the status from the DOM when playback has ended:
+  doorbell.addEventListener('ended', function() {
+    $('#status').remove();
+  });
+
   function ringDoorbell() {
     doorbell.play();
   }
