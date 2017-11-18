fc2d8da9c983ab793e3b46a84304ccf0ad0b26e2
Karl Stolley <karl.stolley@gmail.com>
2017-11-18 13:54:58 -0600

    Bonus task two: ring the doorbell on 'd' keypress
    I'm registering this event on the entire document, and using the `key`
    property on the event to play the sound only on the 'd' key. See this
    MDN doc on KeyboardEvent.key:
    https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key

--- pp-05/site.js
+++ pp-05/site.js
@@ -7,4 +7,9 @@
   $('figure').remove(); // remove the HTML
   $('body').append('<button id="doorbell">Ring the Doorbell</button>'); // cheat a bit and use button
   $('#doorbell').on('click', ringDoorbell);
+  $(document).keypress(function(e) {
+    if(e.key === 'd') {
+      ringDoorbell();
+    }
+  });
 })(jQuery);
