aca0565203234becc1c2762fbdb13704bc031f56
Karl Stolley <karl.stolley@gmail.com>
2017-11-18 13:25:21 -0600

    Add event listener on #doorbell to play sound

--- pp-05/site.js
+++ pp-05/site.js
@@ -6,4 +6,5 @@ function ringDoorbell() {
 (function($) {
   $('figure').remove(); // remove the HTML
   $('body').append('<button id="doorbell">Ring the Doorbell</button>'); // cheat a bit and use button
+  $('#doorbell').on('click', ringDoorbell);
 })(jQuery);
