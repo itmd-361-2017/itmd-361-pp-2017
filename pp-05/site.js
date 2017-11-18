e73691593b154ba5cfa56d4dff4774317cabd34a
Karl Stolley <karl.stolley@gmail.com>
2017-11-18 13:26:11 -0600

    Move ringDoorbell inside the self-executing func
    This silences the linter's complaint about an implicit global.

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,9 +1,9 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
-
 (function($) {
+  function ringDoorbell() {
+    var doorbell = new Audio('media/doorbell.mp3');
+    doorbell.play();
+  }
+
   $('figure').remove(); // remove the HTML
   $('body').append('<button id="doorbell">Ring the Doorbell</button>'); // cheat a bit and use button
   $('#doorbell').on('click', ringDoorbell);
